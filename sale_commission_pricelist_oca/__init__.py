78f62a37a187f2744b3c832efa3e00593cfa7854
Borruso <gborruso@dinamicheaziendali.it>
2025-08-29 17:28:24 +0200

    [MIG] sale_commission_pricelist: Migration to 18.0 + rename sale_commission_pricelist_oca

--- sale_commission_pricelist_oca/__init__.py
+++ sale_commission_pricelist_oca/__init__.py
@@ -1,1 +1,9 @@
 from . import models
+
+from openupgradelib import openupgrade
+
+
+def _sale_commission_pricelist_oca_pre_init_hook(env):
+    if openupgrade.table_exists(env.cr, "sale_commission_pricelist"):
+        modules = [("sale_commission_pricelist", "sale_commission_pricelist_oca")]
+        openupgrade.update_module_names(env.cr, modules, merge_modules=True)
--- sale_commission_pricelist_oca/__manifest__.py
+++ sale_commission_pricelist_oca/__manifest__.py
@@ -2,13 +2,19 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
-    "name": "Sales commissions by pricelist",
-    "version": "17.0.1.0.0",
-    "author": "Tecnativa, " "Odoo Community Association (OCA), " "ArcheTI",
+    "name": "Sales commissions by pricelist OCA",
+    "version": "18.0.1.0.0",
+    "author": "Tecnativa, Odoo Community Association (OCA), ArcheTI",
     "category": "Sales Management",
     "website": "https://github.com/OCA/commission",
     "license": "AGPL-3",
-    "depends": ["sale_commission"],
+    "external_dependencies": {
+        "python": [
+            "openupgradelib",
+        ],
+    },
+    "depends": ["sale_commission_oca"],
     "data": ["views/product_pricelist_view.xml"],
+    "pre_init_hook": "_sale_commission_pricelist_oca_pre_init_hook",
     "installable": True,
 }
